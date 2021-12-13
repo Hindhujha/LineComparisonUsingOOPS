d3a71537cb081a593b225b2c0c7c922087a60e69
unknown <hindhujhasridhar@gmail.com>
2021-12-13 22:42:00 +0530

    LENGTH OF A LINE

---
+++ LineComparison/Length/Length_2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison.Length
+{
+    internal class Length_2
+    {
+        public int X1, X2, Y1, Y2;
+        public void Linelength()
+        {
+            Console.WriteLine("WELCOME TO LINE COMPARISON PROBLEM");
+          
+            Console.WriteLine("ENTER THE COORDINATES:");
+            X1 = Convert.ToInt16(Console.ReadLine());
+            X2 = Convert.ToInt16(Console.ReadLine());
+            Y1 = Convert.ToInt16(Console.ReadLine());
+            Y2 = Convert.ToInt16(Console.ReadLine());
+            
+        }
+
+        public void answer()
+        {
+            var result = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+            Double length = Math.Round((Double)result, 2);
+            Console.WriteLine("LENGTH OF THE LINE: " + length);
+        }
+        }
+
+    }
+
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -6,6 +6,27 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO LINE COMPARISON PROBLEM");
+            while (true)
+            {
+                Console.WriteLine("-----------------");
+                Console.WriteLine("1.LENGTH OF A LINE");
+                Console.WriteLine("ENTER YOUR OPTION");
+
+                int select = Convert.ToInt32(Console.ReadLine());
+
+                switch (select)
+                {
+                    case 1:
+                        Length.Length_2 myObj = new Length.Length_2();
+                        myObj.Linelength();
+                        myObj.answer();
+                        break;
+                    default:
+                        Console.WriteLine("ENTER A CORRECT OPTION");
+                        break;
+
+                }
+            }
         }
     }
 }
