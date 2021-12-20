555a6b7e35ddbb07fbcef609879c7dd4e5e3ae07
unknown <hindhujhasridhar@gmail.com>
2021-12-20 22:04:26 +0530

    CHECK LINES ARE EQUAL/GREATER/LESSER

---
+++ LineComparison/Comparison/Compare.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison.Comparison
+{
+    internal class Compare
+    {
+        public int length1, length2;
+        public void Comparison()
+        {
+
+            Equals.Equal equal = new Equals.Equal();
+            equal.Equals();
+            Console.WriteLine("LENGTH OF THE LINE: " + length1);
+
+         
+        Console.WriteLine("LENGTH OF THE LINE: " + length2);
+
+            if (length1.CompareTo(length2) == 0)
+            {
+                Console.WriteLine("TWO LINES ARE EQUAL");
+            }
+            if (length1.CompareTo(length2) < 0)
+
+            {
+                Console.WriteLine("LESSER THAN LENGTH2");
+            }
+
+if (length1.CompareTo(length2) > 0)
+
+{
+    Console.WriteLine("GREATER THAN LENGTH2");
+}
+
+        }
+    }
+    }
+
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -11,6 +11,7 @@ namespace LineComparison
                 Console.WriteLine("-----------------");
                 Console.WriteLine("1.LENGTH OF A LINE");
                 Console.WriteLine("2.TWO LINES ARE EQUAL OR NOT");
+                Console.WriteLine("3.CHECKING LINES ARE GREATER/LESSER/EQUAL");
                 Console.WriteLine("ENTER YOUR OPTION");
 
                 int select = Convert.ToInt32(Console.ReadLine());
@@ -25,7 +26,11 @@ namespace LineComparison
                     case 2:
                         Equals.Equal equal= new Equals.Equal();
                         equal.Equals();
-                        equal.calc(); 
+                        equal.calc();
+                        break;
+                    case 3:
+                        Comparison.Compare comp=new Comparison.Compare();
+                        comp.Comparison();  
                         break;
                     default:
                         Console.WriteLine("ENTER A CORRECT OPTION");
